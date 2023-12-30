299424dae62ef7a228acd9a39b9e01a70b879775
CatmanFan <30674270+CatmanFan@users.noreply.github.com>
2023-12-30 14:34:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // ==UserScript==
-// @name        Revert X to Twitter Embed
-// @namespace   https://github.com/CatmanFan/Revert-X-to-Twitter-Embed/
-// @icon        https://www.iconarchive.com/download/i76545/xenatt/minimalism/App-Twitter.256.png
-// @author      Mr._Lechkar / CatmanFan
-// @description Fixes Twitter/X embed by reverting X references back to original Twitter ones.
-// @version     0.1
-// @match       https://platform.twitter.com/embed/Tweet.html
-// @run-at      document-start
-// @grant       GM_addStyle
+// @license        GNU GPLv3
+// @name           Revert X Embed back to Twitter Embed
+// @name:fr        Nom et logo originaux de Twitter sur les embeds (contenus externes embarqués)
+// @name:es        Revertir X a Twitter en los Tweets insertados
+// @namespace      https://github.com/CatmanFan/Revert-X-to-Twitter-Embed/
+// @icon           https://www.iconarchive.com/download/i76545/xenatt/minimalism/App-Twitter.256.png
+// @author         Mr._Lechkar / CatmanFan
+// @description    Replaces X references in the Twitter/X embed with the original Twitter ones.
+// @description:fr Modifier les embeds (contenus externes embarqués) de Twitter/X en replaçant les références à X par les originales de Twitter (par exemple, « Poster sur X » devient désormais « Tweeter sur Twitter »).
+// @description:es Reemplazar el logotipo de X y sus otras referencias en los Tweets insertados (embeds) por las originales de Twitter.
+// @version        0.25
+// @match          https://platform.twitter.com/embed/Tweet.html
+// @run-at         document-start
+// @grant          GM_addStyle
+// @downloadURL https://update.greasyfork.org/scripts/481893/Revert%20X%20Embed%20back%20to%20Twitter%20Embed.user.js
+// @updateURL https://update.greasyfork.org/scripts/481893/Revert%20X%20Embed%20back%20to%20Twitter%20Embed.meta.js
 // ==/UserScript==
 
 // Original color (copied from Yakisova's X to Twitter userscript)
@@ -32,11 +39,11 @@ function revertString(item) {
 }
 
 function revertXLogo() {
-  SVG = document.getElementsByClassName("r-18jsvk2 r-4qtqp9 r-yyyyoo r-6zzn7w r-19fsva8 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-q1j0wu")[0].querySelector('g').querySelector('path');
+  SVG = document.getElementsByClassName("r-18jsvk2 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr")[0].querySelector('g').querySelector('path');
+  SVG.setAttribute('d', birdPath);
 
-  if (SVG.getAttribute('d') == xPath) {
-    SVG.setAttribute('d', birdPath);
-  }
+  SVG = document.getElementsByClassName("r-jwli3a r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03")[2].querySelector('g').querySelector('path');
+  SVG.setAttribute('d', birdPath);
 }
 
 function revertHover() {
@@ -65,7 +72,16 @@ function revertStrings() {
     if (item) { item.innerHTML = revertString(item.innerHTML); }
   }
 
+  // "View on X"
+  items = document.getElementsByClassName("css-901oao r-jwli3a r-1qd0xha r-1gkfh8e r-16dba41 r-56xrmm r-146iojx r-bcqeeo r-s1qlax r-1vvnge1 r-qvutc0");
+
+  for (itemX of items) {
+    item = itemX.getElementsByClassName("css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0")[0];
+    if (item) { item.innerHTML = revertString(item.innerHTML); }
+  }
+
   // Same for Accessibility Labels
+  // NOTE: Not fully working at the present moment
   /* items = document.querySelectorAll('body *');
 
   for (item of items) {
@@ -78,6 +94,18 @@ function revertStrings() {
   } */
 }
 
+// Delete "Not found" error, which was not present before Musk takeover iirc
+function deleteNotFound()
+{
+  const txt = document.documentElement.innerHTML;
+
+  if (txt.includes("M12 20.25c4.56 0 8.25-3.69 8.25-8.25S16.56 3.75 12 3.75 3.75 7.44 3.75 12s3.69 8.25 8.25 8.25zM22.25 12c0 5.66-4.59 10.25-10.25 10.25S1.75 17.66 1.75 12 6.34 1.75 12 1.75 22.25 6.34 22.25 12zM10.5 15.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5S12.83 17 12 17s-1.5-.67-1.5-1.5zm.5-2.8L10.75 7h2.5L13 12.7s-.5-.2-1-.2-1 .2-1 .2z") && txt.includes(">Not found</span>"))
+  {
+    document.documentElement.innerHTML = "";
+  }
+}
+
 setInterval(revertXLogo, 1);
 setInterval(revertHover, 1);
-setInterval(revertStrings, 5);
+setInterval(revertStrings, 1);
+setInterval(deleteNotFound, 1);
